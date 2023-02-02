78bc097be649d432a704fded499cdf7c5ae7ba96
Aniket Patel <patelaniket1207@gmail.com>
2023-02-02 21:57:25 +0530

    Added Background particles

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Particle from './Particles';
+
+function App() {
+  return (
+   <>
+    check text 
+    <Particle />
+
+    <div className="check-text">
+      <p>
+        Check text
+      </p>
+    </div>
+   </>
+  );
+}
+
+export default App;
